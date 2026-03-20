2112b69e36918b849b075e2da3b2f07e05e0c3d2
Bohdan <bogdanbelyj@gmail.com>
2026-03-19 21:59:38 -0600

    Solution

--- main.py
+++ main.py
@@ -2,9 +2,39 @@ import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
+import json
+
 
 def main() -> None:
-    pass
+    with open("players.json") as f:
+        players_data = json.load(f)
+
+    for key, value in players_data.items():
+        race = Race.objects.create(
+            name=value["race"]["name"],
+            description=value["race"]["description"]
+        )
+        for skill in value["race"]["skills"]:
+            skills = Skill.objects.create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race,
+            )
+
+        guild = Guild.objects.create(
+            name=value["guild"]["name"],
+            description=value["guild"]["description"],
+        )
+
+        player = Player.objects.create(
+            nickname=key,
+            email=value["email"],
+            bio=value["bio"],
+            race=race,
+            guild=guild,
+        )
+
+    print(race, skills, guild, player)
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import os
+from doctest import master
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
